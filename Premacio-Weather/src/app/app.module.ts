e69f4c16e8d0921d0254564cf16d8644341faf62
Paul Joshua Premacio <pauljoshuapremacio@gmail.com>
2022-03-12 02:47:22 +0800

    Adding funcs

--- Premacio-Weather/src/app/app.module.ts
+++ Premacio-Weather/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { WeatherWidgetMainComponent } from './Components/weather-widget-main/weather-widget-main.component';
 
@@ -10,7 +10,8 @@ import { WeatherWidgetMainComponent } from './Components/weather-widget-main/wea
     WeatherWidgetMainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
